89faa4e076aeed1059d9c9be0b0f057ad31f5014
Welcome <Welcome@DESKTOP-PO557JJ>
2021-07-25 21:56:10 +0700

    first

---
+++ nam/src/testgit/SPHinhtron.java
@@ -0,0 +1,23 @@
+package testgit;
+import java.util.Scanner;
+public class SPHinhtron {
+	
+	public class nhom13 {
+		class DienTichChuViHT {
+		    public  double tinhDienTich(double r) {
+		        return r * r * 3.14;
+		    }
+		    public  double tinhChuVi(double r){
+		        return r * 2 * 3.14;
+		    }
+		    public  void main(String[] args) {
+		        double r;
+		        Scanner scanner = new Scanner(System.in);
+		        System.out.println("Nhập bán kính hình tròn: ");
+		        r = scanner.nextDouble();
+		        System.out.println("Diện tích hình tròn là : " + tinhDienTich(r));
+		        System.out.println("LeVietTinh-update ");
+		}
+		}
+	}
+}
