b4db37ae8ec228219571f44db71a55876d168f0c
Tính Lê <84431347+Phongthanak@users.noreply.github.com>
2021-07-25 20:24:55 +0700

    Update nhom13.java

--- Test/src/Nhom13/nhom13.java
+++ Test/src/Nhom13/nhom13.java
@@ -14,6 +14,7 @@ public class nhom13 {
 	        System.out.println("Nhập bán kính hình tròn: ");
 	        r = scanner.nextDouble();
 	        System.out.println("Diện tích hình tròn là: " + tinhDienTich(r));
+		     System.out.println("LeVietTinh");
 	}
 	}
 }
